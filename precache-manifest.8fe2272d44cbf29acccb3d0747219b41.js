6bd4f0a2097b83fbb9943fa42fdc1904ae3efe6a
Lukas Vanagas <>
2020-02-11 00:56:10 +0200

    Updates

---
+++ precache-manifest.8fe2272d44cbf29acccb3d0747219b41.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78702719d40da1b46afff1985ae9b88",
+    "url": "/luminour-react-challenge/index.html"
+  },
+  {
+    "revision": "6400069d783a5c1c892a",
+    "url": "/luminour-react-challenge/static/js/0.13541271.chunk.js"
+  },
+  {
+    "revision": "324d3f9d63e31888d576",
+    "url": "/luminour-react-challenge/static/js/10.484c365b.chunk.js"
+  },
+  {
+    "revision": "bc5f91cf1cfb89f039e8",
+    "url": "/luminour-react-challenge/static/js/11.e6c15bef.chunk.js"
+  },
+  {
+    "revision": "9a245446a8561cfe0fa5",
+    "url": "/luminour-react-challenge/static/js/3.dae2b6da.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/luminour-react-challenge/static/js/3.dae2b6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "032203598dc014680423",
+    "url": "/luminour-react-challenge/static/js/4.4c1a175b.chunk.js"
+  },
+  {
+    "revision": "dadfff5660eaa099ed33",
+    "url": "/luminour-react-challenge/static/js/5.e3b5ddca.chunk.js"
+  },
+  {
+    "revision": "1c4c4ad94b35fc6f012a",
+    "url": "/luminour-react-challenge/static/js/6.1ef573cb.chunk.js"
+  },
+  {
+    "revision": "e7f7047dc2bc4eb58c2b",
+    "url": "/luminour-react-challenge/static/js/7.f7f23a4a.chunk.js"
+  },
+  {
+    "revision": "73d01ff4a8e192d98335",
+    "url": "/luminour-react-challenge/static/js/8.31c96f1f.chunk.js"
+  },
+  {
+    "revision": "24028aa54cc45c409a3f",
+    "url": "/luminour-react-challenge/static/js/9.39133a31.chunk.js"
+  },
+  {
+    "revision": "117649eded12900a2ce1",
+    "url": "/luminour-react-challenge/static/js/main.a62dbba9.chunk.js"
+  },
+  {
+    "revision": "4a0618bbb7f7def6e836",
+    "url": "/luminour-react-challenge/static/js/runtime-main.0ac756e5.js"
+  }
+]);
